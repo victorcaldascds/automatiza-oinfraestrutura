c1f6e313300f37208c1130c9b8c2cf53d54224b4
Victor Caldas <61563780+victorcaldascds@users.noreply.github.com>
2022-09-26 10:11:51 -0300

    Add files via upload

---
+++ update new_script_without_create_user_or_rename_computer.bat
@@ -0,0 +1,76 @@
+@echo off
+echo ========================================================================
+echo Vamos iniciar o processo de instalacao em lote, fique atento a todas as solicitacoes!
+echo ========================================================================
+echo ================================
+systeminfo | findstr /c:"OS Name"
+systeminfo | findstr /c:"OS Version"
+systeminfo | findstr /c:"System Type"
+:: Section 2: Hardware information.
+echo ===============================
+echo Informacoes de Hardware
+echo ===============================
+systeminfo | findstr /c:"Total Physical Memory"
+wmic cpu get name
+:: Section 3: Networking information.
+ECHO ============================
+ECHO Informacoes de REDE
+ECHO ============================
+ipconfig | findstr IPv4
+PAUSE
+
+cls
+:: Instalacao CHOCO :
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+echo Chocolatey Instalado com Sucesso! Pressione qualquer tecla para continuar!
+
+choco install anydesk.install -y
+choco install googlechrome -y
+choco install slack -y
+choco install dopdf -y
+choco install lightshot -y
+
+echo Instalacao concluida, agora vamos para a parte da pergunta de qual notebook estamos formando.
+pause
+
+cls
+:menu
+cls
+echo Usuario Logado: %username%  Computador:%computername%
+date /t
+
+echo O Notebook que esta sendo formatado eh Dell ou Lenovo? Se for Dell, digite dell e se for Lenovo, digite lenovo.
+echo ________________________________________
+set /p notebook= dell ou lenovo:
+echo ________________________________________
+if %notebook% equ dell goto notebook1
+if %notebook% equ lenovo goto notebook2
+
+:notebook1
+cls
+bitsadmin /transfer DellSupport /priority normal https://downloads.dell.com/serviceability/catalog/SupportAssistInstaller.exe "C:\Users\%username%\Downloads\SupportAssistInstaller.exe" 
+PAUSE
+echo Aperte para instalar
+start C:\Users\"%username%"\Downloads\SupportAssistInstaller.exe
+PAUSE
+echo Ja finalizamos as instalacoes Dell Support Assist, seria interessante reiniciar a maquina e executar o windows update!!!
+pause
+exit
+:notebook2
+cls
+bitsadmin /transfer LenovoSupportAssist /priority normal https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.07.0139.exe "C:\Users\%username%\Downloads\system_update_5.07.0139.exe" 
+PAUSE
+echo Aperte para instalar
+start C:\Users\"%username%"\Downloads\system_update_5.07.0139.exe
+PAUSE
+echo Ja finalizamos as instalacoes do Lenovo Support Assist, seria interessante reiniciar a maquina e executar o windows update!!!
+pause
+exit
+
+:: Instalador configurado pelo Estagiário Victor Caldas dos Santos ::
+pause
+exit
+
+
+
